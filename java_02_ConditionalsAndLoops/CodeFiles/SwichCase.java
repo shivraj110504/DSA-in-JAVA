9e3d34d72056530f4226a8ee3d06d6949a852892
shivraj110504 <tawareshivraj11@gmail.com>
2024-07-02 12:28:34 +0530

    add swich conditions

--- java_02_ConditionalsAndLoops/CodeFiles/SwichCase.java
+++ java_02_ConditionalsAndLoops/CodeFiles/SwichCase.java
@@ -1,4 +1,4 @@
-package java_02_ConditionalsAndLoops;
+package java_02_ConditionalsAndLoops.CodeFiles;
 
 import java.util.Scanner;
 
@@ -6,6 +6,9 @@ public class SwichCase {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String fruit = scan.next();
+        scan.close();
+        
+        //normal swich statement
 
         switch (fruit) {
             case "mango":
@@ -24,6 +27,15 @@ public class SwichCase {
             default:
             System.out.println("please entre valid fruit");
                 break;
+        }
+
+        //enhanced swich statement
+
+        switch (fruit) {
+            case "mango" -> System.out.println("king of fruit");
+            case "apple" -> System.out.println("sweet and red");
+            case "orange" -> System.out.println("round fruit");
+            default -> System.out.println("please entre valid fruit");
         }
 
     }
