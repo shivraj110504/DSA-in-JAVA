c8964d913524bba02698797542e29c413c198499
shivraj110504 <tawareshivraj11@gmail.com>
2024-07-08 22:09:04 +0530

    add electricity bill calculator

---
+++ java_02_ConditionalsAndLoops/Assignment03/electricBill.java
@@ -0,0 +1,24 @@
+package java_02_ConditionalsAndLoops.Assignment03;
+
+// 02 calculate Electicity bill
+
+import java.util.Scanner;
+
+public class ecectricBill {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Your daily usage in Wats (24 hr usage)");
+        float n = scan.nextFloat();
+        System.out.println("Rate : ");
+        float m = scan.nextFloat();
+        System.out.println("No. of days you want to calculate: ");
+        float d = scan.nextFloat();
+        scan.close();
+
+        float electricity = n /1000;
+        electricity = electricity * m * d;
+
+        System.out.println("Your ammout is " + electricity);
+    }
+}
